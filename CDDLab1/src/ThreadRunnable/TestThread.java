e52b4f783d24dd2052821babb2b44ae1e9f3c6a5
Danny D <dannydbob@hotmail.com>
2021-10-04 11:10:32 +0100

    Added Lab1 ThreadDemo and RunnableDemo Classes to project

---
+++ CDDLab1/src/ThreadRunnable/TestThread.java
@@ -0,0 +1,24 @@
+package ThreadRunnable;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import ThreadRunnable.RunnableDemo;
+
+/**
+ *
+ * @author Daniel Dinelli
+ *
+ */
+public class TestThread {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.start();
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.start();
+    }
+
+}
