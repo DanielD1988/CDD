4b1412f1b638068f13581025d7fdefce70b9b4b1
Daniel <51062219+DanielD1988@users.noreply.github.com>
2022-01-17 19:36:44 +0000

    Update mandelbroit.cpp
    small fix

--- CDDLab10/map/mandelbroit.cpp
+++ CDDLab10/map/mandelbroit.cpp
@@ -1,3 +1,49 @@
+// mandelbroit.cpp --- m
+// 
+// Filename: mandelbroit.cpp
+// Description: 
+// Author: Joseph
+// Maintainer: 
+// Created: Mon Feb  4 09:40:41 2019 (+0000)
+// Version: 
+// Package-Requires: ()
+// Last-Updated: Mon Feb  4 10:08:24 2019 (+0000)
+//           By: Joseph
+//     Update #: 18
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
 **
  *
  * @author Daniel Dinelli
